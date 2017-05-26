d186868d41a1c60fbbe7f30eac4d9a7eafc4a909
Nikita <nks.off@gmail.com>
2017-05-26 16:06:52 +0300

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,6 @@
 
 echo "Start deploy.sh..."
 docker login --username=_ --password=$(echo $API_KEY) registry.heroku.com
-if [[ "$1" = "prod" ]]
-then
-    echo "ENV: production"
-    docker build -t registry.heroku.com/vstaem/web .
-    docker push registry.heroku.com/vstaem/web
-else
-    echo "ENV: development"
-    docker build -t registry.heroku.com/vstaem-dev/web .
-    docker push registry.heroku.com/vstaem-dev/web
-fi
+echo "Deploy app: $1"
+docker build -t registry.heroku.com/$1/web .
+docker push registry.heroku.com/$1/web
