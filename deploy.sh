732dfd090bc0f5f93fbe2aa28fe32ee74771f663
Nikita <nks.off@gmail.com>
2017-05-26 16:03:39 +0300

    fix sh warning

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Start deploy.sh..."
 docker login --username=_ --password=$(echo $API_KEY) registry.heroku.com
-if [[ $1 = "prod" ]]
+if [[ "$1" = "prod" ]]
 then
     echo "ENV: production"
     docker build -t registry.heroku.com/vstaem/web .
