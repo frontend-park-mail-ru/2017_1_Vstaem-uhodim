1b5c8fcdce06deb0bf1cd8b848d147df4ec770f4
kate <kat-semyonova@mail.ru>
2017-05-26 16:31:58 +0300

    fixed errors

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
+echo "Start deploy.sh..."
 docker login --username=_ --password=$(echo $API_KEY) registry.heroku.com
-if [[ $1 = "prod" ]]
-    then docker build -t registry.heroku.com/vstaem/web .
-    docker push registry.heroku.com/vstaem/web
-  else
-    docker build -t registry.heroku.com/vstaem-dev/web .
-    docker push registry.heroku.com/vstaem-dev/web
-  fi
-
+echo "Deploy app: $1"
+docker build -t registry.heroku.com/$1/web .
+docker push registry.heroku.com/$1/web
